3d243949509f3d622bf6a226c6c01954b828fe94
mikhalkevich <mikhalkevich@ya.ru>
2019-08-08 00:29:51 +0300

    listener

---
+++ public/javascripts/writer.js
@@ -0,0 +1,12 @@
+$(function () {
+    function func() {
+        $.ajax({
+            method: 'get',
+            url: '/listener/more',
+            success: function (data) {
+                $('.empty').append(data);
+            }
+        })
+    }
+    setInterval(func, 1000);
+})
